2ced0d5113098fea50cbe23a242d668fcdafbe36
OTA2000 <ota.yasuhiro@moneyforward.co.jp>
2021-01-25 19:28:01 +0900

    Deploy to production

--- y2000.dev/src/components/Footer/Footer.jsx
+++ y2000.dev/src/components/Footer/Footer.jsx
@@ -38,7 +38,7 @@ const Footer = () => {
         </div>
         <hr />
         <p className="footer__text">
-          © {new Date().getFullYear()} -
+          © {new Date().getFullYear()} -{' '}
           <a href="https://github.com/OTA2000" target="_blank" rel="noopener noreferrer">
             y2000
           </a>
